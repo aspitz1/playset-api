8c931f0cae12a8fc7dbaffb6d3729af0db463e3d
Anna Spitz <aspitzemail@gmail.com>
2022-09-21 21:31:40 -0500

    Update config

--- config/config.js
+++ config/config.js
@@ -16,6 +16,11 @@ module.exports = {
     "host": process.env.DATABASE_HOST,
     "username": process.env.DATABASE_USERNAME,
     "password": process.env.DATABASE_PASSWORD,  
-    "dialect": "postgres"
+    "dialect": "postgres",
+    "dialectOptions": {
+      "ssl": {
+        "rejectUnauthorized": false
+      }
+    }
   }
 }
